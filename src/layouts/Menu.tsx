d85ade4e606f98d4b5afa9a957aa944ae1bf8b3e
Vladimir Kirpichyov <vlasoul@mail.ru>
2023-12-29 13:59:45 +0300

    feat: add menu icons

--- src/layouts/Menu.tsx
+++ src/layouts/Menu.tsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { Row, Col, Typography } from 'antd';
+import { HomeFilled, PlayCircleFilled } from '@ant-design/icons';
 import { ROUTES, privateRoutes } from '../routes/routes';
 import { Context } from '../store/context';
 import { useAuth } from './../firebase/hooks/useAuth';
@@ -8,11 +9,18 @@ import { auth as firebaseAuth } from '../firebase/firebase';
 import { getText, KEYS as TEXT } from '../locales/text';
 
 const { Text } = Typography;
-
-type Menu = [ROUTES, TEXT];
+enum ICONS {
+  HOME,
+  PLAYGROUND,
+}
+const icons = {
+  [ICONS.HOME]: HomeFilled,
+  [ICONS.PLAYGROUND]: PlayCircleFilled,
+};
+type Menu = [ROUTES, TEXT, ICONS];
 const menu: Menu[] = [
-  [ROUTES.HOME, TEXT.HOME],
-  [ROUTES.PLAYGROUND, TEXT.PLAYGROUND],
+  [ROUTES.HOME, TEXT.HOME, ICONS.HOME],
+  [ROUTES.PLAYGROUND, TEXT.PLAYGROUND, ICONS.PLAYGROUND],
 ];
 
 export default function Menu() {
@@ -29,15 +37,18 @@ export default function Menu() {
         paddingTop: '8px',
       }}
     >
-      {menu.map(([menuKey, textKey]: Menu) => {
+      {menu.map(([menuKey, textKey, iconKey]: Menu) => {
+        console.log(iconKey);
         const text = getText(textKey, locale);
         const isPrivate = privateRoutes.indexOf(menuKey as ROUTES) >= 0;
         const borderBottom = pathname === menuKey ? 'solid' : 'none';
+        const Icon = icons[iconKey];
         const MenuItem =
           isPrivate && !isLogged ? (
             <Text disabled>{text} </Text>
           ) : (
             <Link to={menuKey} style={{ borderBottom, padding: '0px' }}>
+              <Icon style={{ padding: '8px' }} />
               {text}
             </Link>
           );

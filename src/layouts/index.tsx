4c97b88551ab9cf0c06cab3ae570b7452d5dc64d
Abdikhaniyev <alisher147741@gmail.com>
2024-01-06 19:26:55 +0600

    fix: Sidebar visible only playground path

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,14 +1,16 @@
 import { Grid, Layout } from 'antd';
-import { Outlet } from 'react-router-dom';
+import { Outlet, matchPath, useLocation } from 'react-router-dom';
 import LayoutFooter from './Footer';
 import LayoutHeader from './Header';
 import LayoutSidebar from './Sidebar';
+import { ROUTES } from '../routes/routes';
 
 const { Content } = Layout;
 const { useBreakpoint } = Grid;
 
 export default function AppLayout() {
   const { md } = useBreakpoint();
+  const { pathname } = useLocation();
 
   const layoutStyle = {
     minHeight: '100vh',
@@ -24,7 +26,7 @@ export default function AppLayout() {
     <Layout style={layoutStyle}>
       <LayoutHeader />
       <Layout>
-        <LayoutSidebar />
+        {matchPath(pathname, ROUTES.PLAYGROUND) && <LayoutSidebar />}
         <Content style={contentStyle}>
           <Outlet />
         </Content>

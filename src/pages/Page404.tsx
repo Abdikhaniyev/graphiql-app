43c16b8de50c6be4ac510ff6770bb443b3c4c7c1
Vladimir Kirpichyov <vlasoul@mail.ru>
2024-01-07 20:16:00 +0300

    feat: add redirect on auth from 404 page

--- src/pages/Page404.tsx
+++ src/pages/Page404.tsx
@@ -1,15 +1,32 @@
+import { useState, useEffect } from 'react';
 import { Button, Result } from 'antd';
 import { useNavigate } from 'react-router-dom';
 import { KEYS as TEXT, getText } from '../locales/text';
 import { useContext } from 'react';
 import { Context } from '../store/context';
+import { useAuth } from '../firebase/hooks/useAuth';
+import { auth as firebaseAuth } from '../firebase/firebase';
+import { KEYS as METHODS } from '../firebase/methods/methods';
+import { ROUTES } from '../routes/routes';
 
 export default function Page404() {
   const navigate = useNavigate();
   const { locale } = useContext(Context);
+  const [method, setMethod] = useState<METHODS>(METHODS.NOTHING);
+  const [user, , , authMethod] = useAuth(firebaseAuth, method);
+
+  useEffect(() => {
+    if (method === METHODS.SIGN_OUT) {
+      authMethod('', '');
+      setMethod(METHODS.NOTHING);
+      navigate(ROUTES.DEFAULT);
+    } else {
+      if (user?.email) navigate(ROUTES.HOME);
+    }
+  }, [user, method]);
 
   const handleBackHome = () => {
-    navigate('/');
+    navigate(ROUTES.HOME);
   };
 
   return (
